d0188d68a9a0b872a60cac8f91166b8460a76cbe
Victor Xavier <xavier12.bc@gmail.com>
2024-12-09 22:40:18 -0300

    Adicionando drivers postgrade

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get install -y php-pgsql
+# Atualiza pacotes e instala extensões necessárias
+apt-get update && apt-get install -y \
+    php-pgsql \
+    php-pdo-pgsql \
+    postgresql-client
